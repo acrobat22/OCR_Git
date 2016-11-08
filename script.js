a7d95d0b9355229a68ff853579dd6692467d5934
acrobat22 <acrobat22@gmail.com>
2016-11-08 12:11:09 +0100

    Ajout div animée

---
+++ script.js
@@ -0,0 +1,15 @@
+$('div').animate({
+    marginLeft: "-200",
+    marginTop: "-200",
+    width: "400",
+    height: "400"
+    // Comment the line below to see difference
+    //,opacity: 0.5
+}, 2000).animate({
+    marginLeft: -150,
+    marginTop: -150,
+    width: 300,
+    height: 300
+    // Comment the line below to see difference
+   //,opacity: 1
+}, 2000);
